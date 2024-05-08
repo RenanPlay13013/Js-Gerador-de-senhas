ee6ad1d97edf64c7ee356a5141fc0ebb84fa08c0
RenanPlay13013 <127144536+RenanPlay13013@users.noreply.github.com>
2024-05-08 19:21:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,70 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
-const simbolos = '!@%*?';
-
+const simbolos = '!@%*?-+=0()';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
 
-botoes[0].addEventListener('click', diminuiTamanho);
-botoes[1].addEventListener('click', aumentaTamanho);
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-
 function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
+    if (tamanhoSenha < 27) {
+        // tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
-}
+} 
 
-checkbox.forEach(check => {
-    check.addEventListener('change', geraSenha);
-});
+
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
 
 geraSenha();
 
 function geraSenha() {
     let alfabeto = '';
     if (checkbox[0].checked) {
-        alfabeto += letrasMaiusculas;
+        alfabeto = alfabeto + letrasMaiusculas;
     }
     if (checkbox[1].checked) {
-        alfabeto += letrasMinusculas;
+        alfabeto = alfabeto + letrasMinusculas;
     }
     if (checkbox[2].checked) {
-        alfabeto += numeros;
+        alfabeto = alfabeto + numeros;
     }
     if (checkbox[3].checked) {
-        alfabeto += simbolos;
+        alfabeto = alfabeto + simbolos;
     }
-
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
-        senha += alfabeto[numeroAleatorio];
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
     }
-
     campoSenha.value = senha;
     classificaSenha(alfabeto.length);
+
 }
 
 function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
